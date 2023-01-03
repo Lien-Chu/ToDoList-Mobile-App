d6b82bd34122760d3931902f4db5ce3711f5a0a2
Lien-Chu Huang <liarn2001@gmail>
2023-01-03 04:15:32 +0100

    first commit

---
+++ ToDoList/ToDoList/TodoListViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace ToDoList
+{
+    public class TodoListViewModel
+    {
+        public ObservableCollection<TodoItem> TodoItems { get; set; }
+        public TodoListViewModel()
+        {
+            TodoItems = new ObservableCollection<TodoItem>();
+
+            TodoItems.Add(new TodoItem("Todo 1", false));
+            TodoItems.Add(new TodoItem("Todo 2", false));
+            TodoItems.Add(new TodoItem("Todo 2", false));
+        }
+
+        public ICommand AddTodoCommand => new Command(AddTodoItem);
+        public string NewTodoInputValue { get; set; }
+        void AddTodoItem()
+        {
+            TodoItems.Add(new TodoItem(NewTodoInputValue, false));
+        }
+        public ICommand RemoveTodoCommand => new Command(RemoveTodoItem);
+        void RemoveTodoItem(object o)
+        {
+            TodoItem todoItemBeingRemoved = o as TodoItem;
+            TodoItems.Remove(todoItemBeingRemoved);
+        }
+
+    }
+}
---
+++ ToDoList/ToDoList/TodoLtem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToDoList
+{
+   
+    public class TodoItem
+    {
+        public string TodoText { get; set; }
+        public bool Complete { get; set; }
+
+        public TodoItem(string TodoText, bool Complete)
+        {
+            this.TodoText = TodoText;
+            this.Complete = Complete;
+        }
+    }
+}
